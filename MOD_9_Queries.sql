48e800121c3117fce367111c9c6acd61306bfc12
Daniel Rodriguez <111673881+Dr0d1536@users.noreply.github.com>
2023-02-13 17:21:41 -0800

    Add files via upload

--- MOD_9_Queries.sql
+++ MOD_9_Queries.sql
@@ -2,58 +2,62 @@
 
 -- ALTER TABLE employees
 -- ALTER COLUMN hire_date TYPE DATE
--- USING hire_date::date;
+-- USING hire_date::DATE;
 -- CREATE TABLE employees (
--- 	emp_no INT PRIMARY KEY,
+-- 	emp_no INT(255) PRIMARY KEY,
 -- 	emp_title_id CHAR(255),
 -- 	birth_date DATE,
--- 	first_name VARCHAR,
--- 	last_name VARCHAR,
--- 	sex VARCHAR,
+-- 	first_name VARCHAR(255),
+-- 	last_name VARCHAR(255),
+-- 	sex VARCHAR(255),
 -- 	hire_date DATE
 -- );
 
 
 -- CREATE TABLE departments (
--- 	dept_no VARCHAR PRIMARY KEY,
--- 	dept_name VARCHAR
+-- 	dept_no VARCHAR(255) PRIMARY KEY,
+-- 	dept_name VARCHAR(255)
 -- );
 
 -- DROP TABLE dept_emp;
 -- CREATE TABLE dept_emp (
--- 	emp_no INT,
--- 	dept_no VARCHAR,
+-- 	emp_no INT(255),
+-- 	dept_no VARCHAR(255),
 -- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 -- 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 -- );
 
 
 -- CREATE TABLE dept_manager (
--- 	dept_no VARCHAR,
--- 	emp_no INT,
+-- 	dept_no VARCHAR(255),
+-- 	emp_no INT(255),
 -- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 -- 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 -- );
 
 
 -- CREATE TABLE salaries (
--- 	emp_no INT,
--- 	salary INT,
+-- 	emp_no INT(255),
+-- 	salary INT(255),
 -- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 -- );
 
 -- DROP TABLE titles
 -- CREATE TABLE titles (
--- 	title_id VARCHAR PRIMARY KEY,
--- 	title VARCHAR
+-- 	title_id VARCHAR(255) PRIMARY KEY,
+-- 	title VARCHAR(255)
 -- );
 
 -- List the employee number, last name, first name, sex, and salary of each employee
 
--- SELECT *
--- FROM employees
--- LEFT JOIN salaries
--- on employees.emp_no = salaries.emp_no;
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+employees.sex,
+salaries.salary
+FROM employees
+LEFT JOIN salaries
+on employees.emp_no = salaries.emp_no;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986
 
@@ -61,4 +65,74 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE extract(year from hire_date) = 1986
 
+-- List the manager of each department along with their 
+-- department number, department name, employee number, last name, and first name
 
+SELECT departments.dept_name,
+dept_manager.dept_no,
+dept_manager.emp_no,
+employees.first_name,
+employees.last_name
+FROM dept_manager
+LEFT JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees 
+ON dept_manager.emp_no = employees.emp_no;
+
+-- List the department number for each employee along with that employee’s 
+-- employee number, last name, first name, and department name 
+
+SELECT
+employees.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name,
+dept_emp.dept_no
+FROM employees 
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no=dept_emp.dept_no;
+
+
+-- List first name, last name, and sex of each employee whose first name 
+-- is Hercules and whose last name begins with the letter B
+
+SELECT * 
+FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+-- List each employee in the Sales department, including their 
+-- employee number, last name, and first name
+
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales';
+
+-- List each employee in the Sales and Development departments, including their 
+-- employee number, last name, first name, and department name
+
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+departments.dept_name
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments _
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name in ('Sales', 'Development')
+
+-- List the frequency counts, in descending order, of all the 
+-- employee last names (that is, how many employees share each last name)
+
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
